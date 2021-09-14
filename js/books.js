6d43e55c1ec54f560a84729d6d046116ca6e6aec
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-09-14 14:11:00 +0200

    Create a function to add a new book to the collection, with title and author

---
+++ js/books.js
@@ -0,0 +1,33 @@
+const booksList = document.getElementById('booksList');
+const addBookForm = document.getElementById('addBookForm');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const addBtn = document.querySelector('#addBtn');
+let getRemoveButtons = [];
+let collectionOfBooks = [];
+
+
+const book = {
+  title: '',
+  author: '',
+};
+
+/********************************************************* 
+*** bookObject and localStorage manipulation Functions *** 
+**********************************************************/
+function addBook() {
+  let id = 0;
+  if (title.value.length < 1 || author.value < 1) {
+    return;
+  }
+  if (collectionOfBooks.length > 0) {
+  // collectionOfBooks [[id, BookObj] [id, BookObj] ...] 
+  // checks the collectionOfBooks last array, first element: id and adds 1
+  id = (collectionOfBooks[collectionOfBooks.length -1][0]) + 1;
+  } 
+  const currentBook = Object.create(book);
+  currentBook.title = title.value;
+  currentBook.author = author.value;
+  collectionOfBooks.push([id, currentBook]);
+}
+
